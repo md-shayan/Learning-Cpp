39576624e10e2116ff420b29662829c9f0cfcc81
md-shayan <mshayan3406@gmail.com>
2023-08-15 14:59:51 +0530

    modified:   Code/Data Structures And Algorithms/Stack/Stack.h

--- Code/Data Structures And Algorithms/Stack/Stack.h
+++ Code/Data Structures And Algorithms/Stack/Stack.h
@@ -1,140 +1,52 @@
 template <typename T>
+class Node {
+	public:
+	T data;
+	Node<T>* next;
+	Node(T set) {
+		data = set;
+		next = nullptr;
+	}
+};
+
+template <typename T>
 class Stack {
-    T* data;
-    const int maxSize;
-    public:
-    int totalElements = 0;
-    Stack(int size) : maxSize{size} {
-        data = new T[size];
-    }
+	public:
+	Node<T>* topNode;
+	Stack() {topNode = nullptr;}
 
-    // Time Complexity: O(1)
-    bool isEmpty() {
-        return totalElements == 0;
-    }
+	bool empty() {return (topNode == nullptr);}
 
-    // Time Complexity: O(1)
-    void push(T elem) {
-        if (totalElements >= maxSize) {
-            std::cout << "Stack is full" << std::endl;
-            return;
-        }
-        data[totalElements] = elem;
-        totalElements++;
-    }
+	void push(T newElem) {
+		Node<T>* newNode = new Node<T>(newElem);
+		newNode->next = topNode;
+		topNode = newNode;
+	}
 
-    // Time Complexity: O(1)
-    T pop() {
-        if (isEmpty()) {
-            std::cout << "Stack is empty" << std::endl;
-            return 0;
-        }
-        T top = data[totalElements--];
-        return top;
-    }
 
-    // Time Complexity: O(1)
-    T top() {
-        if (isEmpty()) {
-            std::cout << "Stack is empty" << std::endl;
-            return 0;
-        }
-        return data[totalElements - 1];
-    }
+	void push(Node<T>* newNode) {
+		newNode->next = topNode;
+		topNode = newNode;
+	}
 
-    int size() {return totalElements;}
+	void pop() {
+		Node<T>* temp = topNode;
+		topNode = topNode->next;
+		temp = nullptr;
+	}
 
-    // Time Complexity: O(N)
-    void print() {
-        if (isEmpty()) {
-            std::cout << "Stack is empty" << std::endl;
-            return;
-        }
-        std::cout<<data[0];
-        for (int i = 1; i < totalElements; i++) {
-            std::cout <<  " - " << data[i];
-        }
-        std::cout << std::endl;
-    }
-};
+	T top() {return topNode->data;}
 
-template <typename T>
-class StackNode {
-    public:
-    T data;
-    StackNode<T>* next;
-    StackNode(T data) {
-        this->data = data;
-        next = nullptr;
-    }
-};
-
-template <typename T>
-class DynamicStack {
-    StackNode<T>* topNode;
-    int totalNodes = 0;
-    public:
-    DynamicStack() {
-        topNode = nullptr;
-    }
-
-    // Time Complexity: O(1)
-    bool isEmpty() {return topNode == nullptr;}
-
-    // Time Complexity: O(1)
-    void push(T data) {
-        StackNode<T>* newNode = new StackNode<T>(data);
-        newNode->next = topNode;
-        topNode = newNode;
-        totalNodes++;
-    }
-
-    // Time Complexity: O(1)
-    void push(StackNode<T>* newNode) {
-        newNode->next = topNode;
-        topNode = newNode;
-        totalNodes++;
-    }
-
-    // Time Complexity: O(1)
-    T pop() {
-        if (isEmpty()) {
-            std::cout << "Stack is empty" << std::endl;
-            return 0;
-        }
-        T data = topNode->data;
-        StackNode<T>* temp = topNode;
-        topNode = temp->next;
-        temp = nullptr;
-        delete temp;
-        totalNodes--;
-        return data;
-    }
-
-    // Time Complexity: O(1)
-    T top() {
-        if (isEmpty()) {
-            std::cout << "Stack is empty" << std::endl;
-            return 0;
-        }
-        return topNode->data;
-    }
-
-    // Time Complexity: O(1)
-    int size() {return totalNodes;}
-
-    // Time Complexity: O(N)
-    void print() {
-        if (isEmpty()) {
-            std::cout << "Stack is empty" << std::endl;
-            return;
-        }
-        StackNode<T>* temp = topNode;
-        std::cout<<temp->data;
-        while (temp->next!= nullptr) {
-            std::cout <<  " <- " << temp->next->data;
-            temp = temp->next;
-        }
-        std::cout << std::endl;
-    }
+	void print() {
+		if (empty()) {
+			std::cout << "Stack is empty" << std::endl;
+			return;
+		}
+		Node<T>* temp = topNode;
+		while (temp != nullptr) {
+			std::cout << temp->data << " -> ";
+			temp = temp->next;
+		}
+		std::cout << "END" <<std::endl;
+	}
 };
