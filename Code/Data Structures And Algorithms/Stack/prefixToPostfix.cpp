c3811302fec7fa3dec20c95e3408bb6850df0e30
md-shayan <mshayan3406@gmail.com>
2023-08-15 15:07:42 +0530

    modified:   Code/Data Structures And Algorithms/Stack/prefixToPostfix.cpp

--- Code/Data Structures And Algorithms/Stack/prefixToPostfix.cpp
+++ Code/Data Structures And Algorithms/Stack/prefixToPostfix.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <stack>
+#include "Stack.h"
 
 // Iterate over the expression from the last
 // If the current element is NOT an operator, push it to the stack
@@ -9,7 +9,7 @@
 // Time Complexity: O(N)
 // Space Complexity: O(N)
 std::string prefixToPostfix(std::string expression) {
-	std::stack<std::string> st;
+	Stack<std::string> st;
 	char c;
 	std::string newString;
 	for (int i=expression.length()-1; i>=0; i--) {
